eb9dab753cb161107010280691dccbceffac9c92
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-06-06 12:09:01 -0300

    con video 12

---
+++ src/main/java/com/portfolio/jgl/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.jgl.Repository;
+
+import com.portfolio.jgl.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long>{
+
+}
