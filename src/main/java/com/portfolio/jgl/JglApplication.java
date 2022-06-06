f270cc70321d87a5c29937044ceef1761ea135bd
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-06-05 22:01:40 -0300

    Initial commit

---
+++ src/main/java/com/portfolio/jgl/JglApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JglApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JglApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/jgl/JglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JglApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
