0f2354cbe2f35e62614fba3e68c71aa3830862f3
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-29 19:09:28 -0300

    0.4

--- src/main/java/com/portfolio/jgl/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/jgl/Controller/CExperiencia.java
@@ -14,7 +14,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/portfolio/jgl/Controller/PersonaController.java
+++ src/main/java/com/portfolio/jgl/Controller/PersonaController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
