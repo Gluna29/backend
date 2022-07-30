bd9f3f1a8fddfd89f0cc19189105c1ef6813643c
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-30 20:12:04 -0300

    .943

--- src/main/java/com/portfolio/jgl/Controller/PersonaController.java
+++ src/main/java/com/portfolio/jgl/Controller/PersonaController.java
@@ -6,7 +6,6 @@ import com.portfolio.jgl.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
