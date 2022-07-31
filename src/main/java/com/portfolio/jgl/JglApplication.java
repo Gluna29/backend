70c5cf7de0e46c629824f3bfd2cfc820cc8758f1
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-31 09:46:13 -0300

    ddd

--- src/main/java/com/portfolio/jgl/JglApplication.java
+++ src/main/java/com/portfolio/jgl/JglApplication.java
@@ -3,6 +3,7 @@ package com.portfolio.jgl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,10 +16,13 @@ public class JglApplication {
 @Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
+
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("**").allowedMethods("*").allowedHeaders("*");
+
+				registry.addMapping("/**").allowedOrigins("https://apjgl.herokuapp.com");
 			}
 		};
 	}
+	
 }
