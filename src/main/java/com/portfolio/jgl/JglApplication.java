33090b4079a4938ac91aa8d4a52a7589bba589a1
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-30 21:19:41 -0300

    .9445666485

--- src/main/java/com/portfolio/jgl/JglApplication.java
+++ src/main/java/com/portfolio/jgl/JglApplication.java
@@ -17,7 +17,7 @@ public class JglApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("**").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
