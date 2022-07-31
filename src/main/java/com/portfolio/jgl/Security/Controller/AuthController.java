ff25083c0e7ebd197f40008bc1f13bc8038cbd3c
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-31 13:10:26 -0300

    .94456664988j5uuuuuiikju9k8

--- src/main/java/com/portfolio/jgl/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/jgl/Security/Controller/AuthController.java
@@ -32,6 +32,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @RequestMapping("auth")
 @CrossOrigin
