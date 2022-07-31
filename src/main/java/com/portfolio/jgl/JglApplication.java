6bbebbf1602af8909036cd1922df7892ae2d0cec
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-31 10:06:00 -0300

    ddkkd

--- src/main/java/com/portfolio/jgl/JglApplication.java
+++ src/main/java/com/portfolio/jgl/JglApplication.java
@@ -23,7 +23,7 @@ public class JglApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 
-				registry.addMapping("/**").allowedOrigins("https://apjgl.herokuapp.com");
+				registry.addMapping("/**").allowedOrigins("https://apjgl.herokuapp.com/");
 			}
 		};
 	}
@@ -31,7 +31,7 @@ public class JglApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://apjgl.herokuapp.com"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://apjgl.herokuapp.com/"));
 
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
