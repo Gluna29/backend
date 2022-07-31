8bc018e0fd7eb0f4aeb923e80bbc27b886c00d4f
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-31 11:33:25 -0300

    .94456664988j5

--- src/main/java/com/portfolio/jgl/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/jgl/Controller/CExperiencia.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-
+@CrossOrigin(origins = "https://apjglfront.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- src/main/java/com/portfolio/jgl/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/jgl/Security/Controller/AuthController.java
@@ -26,6 +26,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-
+@CrossOrigin
 public class AuthController {
 
     @Autowired
