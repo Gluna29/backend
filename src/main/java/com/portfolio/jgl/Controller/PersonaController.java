e0e75f40a23323776f9887bcc063284699137d50
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-31 20:06:15 -0300

    dd

--- src/main/java/com/portfolio/jgl/Controller/PersonaController.java
+++ src/main/java/com/portfolio/jgl/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-@CrossOrigin(origins = "https://apjglfront.web.app/")
+@CrossOrigin(origins = "https://apjglfront.web.app")
 
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
