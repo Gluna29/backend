78501d9e269707353207bc17b9b14e6fd843ab1b
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-31 12:33:13 -0300

    .94456664988j5uuuuuiiu9

--- src/main/java/com/portfolio/jgl/JglApplication.java
+++ src/main/java/com/portfolio/jgl/JglApplication.java
@@ -1,10 +1,14 @@
 package com.portfolio.jgl;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class JglApplication {
@@ -12,14 +16,31 @@ public class JglApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JglApplication.class, args);
 	}
-@Bean
+
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer(){
+		return new WebMvcConfigurer() {
+
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://apjglfront.web.app").allowedMethods("*").allowedHeaders("*") .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+
+				registry.addMapping("/**").allowedOrigins("https://apjglfront.web.app/");
 			}
 		};
-                
 	}
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("https://apjglfront.web.app/"));
+
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);	}
 }
