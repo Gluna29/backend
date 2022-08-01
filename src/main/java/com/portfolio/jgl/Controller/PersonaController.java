fb5a5d5cae34a7f43d02d0fdf9f9ae57f49a99ed
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-07-31 22:10:53 -0300

    1.0

--- src/main/java/com/portfolio/jgl/Controller/PersonaController.java
+++ src/main/java/com/portfolio/jgl/Controller/PersonaController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-@CrossOrigin(origins = "*")
 
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
--- src/main/java/com/portfolio/jgl/Security/MainSecurity.java
+++ src/main/java/com/portfolio/jgl/Security/MainSecurity.java
@@ -47,7 +47,6 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("**").permitAll()
                 .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
