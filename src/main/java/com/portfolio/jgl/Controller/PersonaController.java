f85f337ad3885ef179a650221c55c7d7333e4ef9
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-08-12 17:36:17 -0300

    2.1us3

--- src/main/java/com/portfolio/jgl/Controller/PersonaController.java
+++ src/main/java/com/portfolio/jgl/Controller/PersonaController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
-     @PreAuthorize("hasRole ('*')")  
+
      @GetMapping("personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
@@ -51,7 +51,7 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         return persona;
     }
-    @PreAuthorize("hasRole ('*')")
+
     @GetMapping("personas/traer/perfil")
     public Persona findPersona(){
         return ipersonaService.findPersona((long)1);
