bc32c0151dbff065c11734ee39b4b3e031449303
Gluna29 <92641973+Gluna29@users.noreply.github.com>
2022-08-22 09:44:53 -0300

    3.31

--- src/main/java/com/portfolio/jgl/Security/MainSecurity.java
+++ src/main/java/com/portfolio/jgl/Security/MainSecurity.java
@@ -11,6 +11,7 @@ import com.portfolio.jgl.Security.jwt.JwtTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -47,7 +48,8 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("**").permitAll()
+                .antMatchers(HttpMethod.GET,"/persona").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
